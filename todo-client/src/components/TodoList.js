0da53563ecf0e8bf7a8f1155a8c0bfd3b5d32396
Jogesh Muppala <jmuppala@hotmail.com>
2020-06-29 21:40:36 +0800

    TodoList Hooks

--- todo-client/src/components/TodoList.js
+++ todo-client/src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -8,41 +8,31 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { fetchTodos, putTodo, deleteTodo } from '../redux/ActionCreators';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import { useSelector, useDispatch } from 'react-redux';
 
-const useStyles = {
+const useStyles = makeStyles({
   root: {
     width: '100%'
   },
-};
-
-const mapStateToProps = state => {
-  return {
-    todos: state.todos
-  }
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  fetchTodos: () => dispatch(fetchTodos()),
-  putTodo: (todo) => dispatch(putTodo(todo)),
-  deleteTodo: (todoId) => dispatch(deleteTodo(todoId))
 });
 
-class TodoList extends Component {
+export default function TodoList () {
 
-  constructor(props) {
-    super(props);
+  const classes = useStyles();
 
-    this.handleToggle = this.handleToggle.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
+  const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch();
 
-  componentDidMount() {
-    this.props.fetchTodos();
-  }
+  console.log(todos);
 
-  handleToggle(todo){
+  useEffect(() => {
+    if (todos.todos === null && todos.errMess === null && !todos.isLoading)
+      dispatch(fetchTodos())
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [todos] );
+
+  const handleToggle = (todo) => () => {
 
     if (todo.completed) {
       todo.completed = false;
@@ -50,92 +40,86 @@ class TodoList extends Component {
       todo.completed = true;
     }
 
-    this.props.putTodo(todo);
+    dispatch(putTodo(todo));
 
+  };
+
+  const handleDelete = (todo) => () => {
+    dispatch(deleteTodo(todo._id));
   }
 
-  handleDelete(todo){
-    this.props.deleteTodo(todo._id);
-  }
-
-  render() {
-
-    const { todos, classes } = this.props;
-
-    if (todos.isLoading) {
-        return(
-            <div className={classes.root}>
-              Loading . . .
-            </div>
-        );
-    }
-    else if (todos.errMess) {
-        return(
+  if (todos.isLoading) {
+      return(
           <div className={classes.root}>
-            <h4>{todos.errMess}</h4>
+            Loading . . .
           </div>
-        );
-    }
-    else if (todos.todos !== null) {
-      return (
-        <React.Fragment>
-          <List className={classes.root}>
-            {todos.todos.filter((todo) => !todo.completed).map((todo) => {
-              return (
-                <ListItem key={todo._id} role={undefined} dense button onClick={() => this.handleToggle(todo)}>
-                  <ListItemIcon>
-                    <Checkbox
-                      edge="start"
-                      checked={todo.completed}
-                      tabIndex={-1}
-                      disableRipple
-                      inputProps={{ 'aria-labelledby': todo._id }}
-                    />
-                  </ListItemIcon>
-                  <ListItemText id={todo._id} primary={todo.title} />
-                  <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="comments" onClick={() => this.handleDelete(todo)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>
-              );
-            })}
-          </List>
-          <List className={classes.root}>
-            {todos.todos.filter((todo) => todo.completed).map((todo) => {
-              return (
-                <ListItem key={todo._id} role={undefined} dense button onClick={() => this.handleToggle(todo)}>
-                  <ListItemIcon>
-                    <Checkbox
-                      edge="start"
-                      checked={todo.completed}
-                      tabIndex={-1}
-                      disableRipple
-                      inputProps={{ 'aria-labelledby': todo._id }}
-                    />
-                  </ListItemIcon>
-                  <ListItemText id={todo._id} style={{textDecoration: 'line-through'}} primary={todo.title} />
-                  <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="comments" onClick={() => this.handleDelete(todo)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>
-              );
-            })}
-          </List>
-        </React.Fragment>
       );
-    }  
-    else {
+  }
+  else if (todos.errMess) {
       return(
         <div className={classes.root}>
-          <h4>Null</h4>
+          <h4>{todos.errMess}</h4>
         </div>
       );
-    }
+  }
+  else if (todos.todos !== null) {
+    return (
+      <React.Fragment>
+        <List className={classes.root}>
+          {todos.todos.filter((todo) => !todo.completed).map((todo) => {
+            return (
+              <ListItem key={todo._id} role={undefined} dense button onClick={handleToggle(todo)}>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={todo.completed}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': todo._id }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={todo._id} primary={todo.title} />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="comments" onClick={handleDelete(todo)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </List>
+        <List className={classes.root}>
+          {todos.todos.filter((todo) => todo.completed).map((todo) => {
+            return (
+              <ListItem key={todo._id} role={undefined} dense button onClick={handleToggle(todo)}>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={todo.completed}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': todo._id }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={todo._id} style={{textDecoration: 'line-through'}} primary={todo.title} />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="comments" onClick={handleDelete(todo)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </List>
+      </React.Fragment>
+    );
+  }  
+  else {
+    return(
+      <div className={classes.root}>
+        <h4>Null</h4>
+      </div>
+    );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(TodoList));
