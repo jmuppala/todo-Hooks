9946ee8c5d8649a0d23ec62905ddf4e2e6918abc
Jogesh Muppala <jmuppala@hotmail.com>
2020-06-29 20:22:15 +0800

    AddTodo Hooks

--- todo-client/src/components/AddTodo.js
+++ todo-client/src/components/AddTodo.js
@@ -1,52 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { postTodo } from '../redux/ActionCreators';
 import TextField from '@material-ui/core/TextField';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import { useDispatch } from 'react-redux';
 
-  const useStyles = {
+  const useStyles = makeStyles({
     root: {
       '& .MuiTextField-root': {
         margin: '30px 0',
-        width: '100%',
-      },
-    },
-  };
-  const mapStateToProps = state => {
-    return {};
-  };
-
-  const mapDispatchToProps = (dispatch) => ({
-    postTodo: (todo) => dispatch(postTodo(todo))
+        width: '100%'
+      }
+    }
   });
 
-class AddTodo extends Component {
+  export default function  AddTodo () {
+    const classes = useStyles();
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoItem: ''
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    const [todoItem, setTodoItem] = useState('');
 
-  handleSubmit(event){
+    const dispatch = useDispatch();
+
+
+    const handleSubmit = (event) => {
       event.preventDefault();
-      this.props.postTodo({title: this.state.todoItem, completed: false});
-      this.setState({
-        todoItem: ''
-      });
-  }
+      dispatch(postTodo({title: todoItem, completed: false}));
+      setTodoItem('');
+    }
 
-  render() {
-    const { classes } = this.props;
     return(
-        <form className={classes.root} noValidate autoComplete="off" onSubmit={this.handleSubmit}>
-            <TextField label="Add To Do Item Here" value={this.state.todoItem} onChange={event => this.setState({todoItem: event.target.value})} />
-        </form>
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField label="Add To Do Item Here" value={todoItem} onChange={event => setTodoItem(event.target.value)} />
+      </form>
     );
-  }
 
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(AddTodo));
